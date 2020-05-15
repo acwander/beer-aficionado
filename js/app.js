7d3a0e1423e0c5edb976d903022d79b81e33bc4f
Alex Wander <alexcwander@gmail.com>
2020-05-15 13:33:26 -0500

    pull style from api

--- js/app.js
+++ js/app.js
@@ -1,14 +1,8 @@
 let endpoint = "beers";
-
-// TODO: get userInput
-// the two bars set the default value
-// let userInput = $('input[type="text"]').val() || 10;
-
-// Getting an error prior to adding cors-anywhere to GET the data
-// https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
 const fixCors = `https://cors-anywhere.herokuapp.com/`;
+const APIKEY = `267e0b1478baa842b500f2a4b17f73c5`;
 // const link = "";
-let link = `${fixCors}https://sandbox-api.brewerydb.com/v2/${endpoint}/?key=267e0b1478baa842b500f2a4b17f73c5`;
+let link = `${fixCors}https://sandbox-api.brewerydb.com/v2/${endpoint}/?key=${APIKEY}`;
 
 $.ajax({
   type: "GET",
@@ -27,6 +21,7 @@ $.ajax({
         key.labels !== undefined
       ) {
         console.log(key.nameDisplay);
+        console.log(key.style.shortName);
         console.log(key.abv);
         console.log(key.description);
         console.log(key.labels.large);
