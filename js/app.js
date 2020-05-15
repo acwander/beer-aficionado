64b16e6fe3ea46360213c1b70cc09c7a20f85158
Alex Wander <alexcwander@gmail.com>
2020-05-15 16:14:52 -0500

    carousel next btn functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let endpoint = "beers";
-const fixCors = `https://cors-anywhere.herokuapp.com/`;
+const fixCORS = `https://cors-anywhere.herokuapp.com/`;
 const APIKEY = `267e0b1478baa842b500f2a4b17f73c5`;
-// let link = `${fixCors}https://sandbox-api.brewerydb.com/v2/${endpoint}/?key=${APIKEY}`;
+// let link = `${fixCORS}https://sandbox-api.brewerydb.com/v2/${endpoint}/?key=${APIKEY}`;
 const link = "./data.json";
 
 $.ajax({
@@ -12,6 +12,13 @@ $.ajax({
   (data) => {
     // console.log(data);
     const dataArr = data.data;
+    // data arrays
+    const name = [];
+    const style = [];
+    const abv = [];
+    const desc = [];
+    const images = [];
+
     for (const key of dataArr) {
       // only pull info of beers that is defined
       if (
@@ -20,13 +27,48 @@ $.ajax({
         key.description !== undefined &&
         key.labels !== undefined
       ) {
-        console.log(key.nameDisplay);
-        console.log(key.style.shortName);
-        console.log(key.abv);
-        console.log(key.description);
-        console.log(key.labels.large);
+        // push data to organized arrays
+        name.push(key.nameDisplay);
+        style.push(key.style.shortName);
+        abv.push(key.abv);
+        desc.push(key.description);
+        images.push(key.labels.large);
       }
     }
+    let currentIndex = 0;
+    const numOfImgs = images.length - 1;
+
+    $(".next-btn").click(function (e) {
+      e.preventDefault();
+
+      $(".carousel-img").children().css("display", "none");
+      $(".beer-name").css("display", "none");
+      $(".beer-style").css("display", "none");
+      $(".beer-abv").css("display", "none");
+      $(".beer-desc").css("display", "none");
+      if (currentIndex < numOfImgs) {
+        currentIndex++;
+      } else {
+        currentIndex = 0;
+      }
+      $("<img>").attr("src", images[currentIndex]).prependTo(".carousel-img");
+
+      $('<h2 class="beer-name">')
+        .text(name[currentIndex])
+        .appendTo("#carousel-info");
+
+      $('<h3 class="beer-style">')
+        .text(style[currentIndex])
+        .appendTo("#carousel-info");
+
+      $('<h3 class="beer-abv">')
+        .text(abv[currentIndex] + "% ABV")
+        .appendTo("#carousel-info");
+
+      $('<p class="beer-desc">')
+        .text(desc[currentIndex])
+        .appendTo("#carousel-info");
+    });
   },
   (err) => {
     console.log("Error", err);
