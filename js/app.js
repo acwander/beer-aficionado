0bfc12b9832adf354072822df38a6577b4edabee
Alex Wander <alexcwander@gmail.com>
2020-05-15 11:57:26 -0500

    working api

--- js/app.js
+++ js/app.js
@@ -1,33 +1,39 @@
 let endpoint = "beers";
 
-// $.ajax({
-//   // Getting an error prior to this solution to pull the data from the API
-//   // https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
-//   url:
-//     "https://cors-anywhere.herokuapp.com/" +
-//     "https://sandbox-api.brewerydb.com/v2/" +
-//     endpoint +
-//     "/?key=267e0b1478baa842b500f2a4b17f73c5",
-// }).then(
-//   (data) => {
-//     // console.log(data);
-//     const dataArr = data.data;
-//     for (const key of dataArr) {
-//       // only pull info of beers that is defined
-//       if (
-//         key.nameDisplay !== undefined &&
-//         key.abv !== undefined &&
-//         key.description !== undefined &&
-//         key.labels !== undefined
-//       ) {
-//         console.log(key.nameDisplay);
-//         console.log(key.abv);
-//         console.log(key.description);
-//         console.log(key.labels.large);
-//       }
-//     }
-//   },
-//   (error) => {
-//     console.log("Error", error);
-//   }
-// );
+// TODO: get userInput
+// the two bars set the default value
+// let userInput = $('input[type="text"]').val() || 10;
+
+// Getting an error prior to adding cors-anywhere to GET the data
+// https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
+const fixCors = `https://cors-anywhere.herokuapp.com/`;
+// const link = "";
+let link = `${fixCors}https://sandbox-api.brewerydb.com/v2/${endpoint}/?key=267e0b1478baa842b500f2a4b17f73c5`;
+
+$.ajax({
+  type: "GET",
+  url: link,
+  dataType: "json",
+}).then(
+  (data) => {
+    // console.log(data);
+    const dataArr = data.data;
+    for (const key of dataArr) {
+      // only pull info of beers that is defined
+      if (
+        key.nameDisplay !== undefined &&
+        key.abv !== undefined &&
+        key.description !== undefined &&
+        key.labels !== undefined
+      ) {
+        console.log(key.nameDisplay);
+        console.log(key.abv);
+        console.log(key.description);
+        console.log(key.labels.large);
+      }
+    }
+  },
+  (err) => {
+    console.log("Error", err);
+  }
+);
